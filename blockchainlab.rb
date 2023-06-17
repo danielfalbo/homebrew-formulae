8fc3524c8d046848a0e489a839181e6e40f82be0
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-06-17 13:44:49 +0200

    blockchainlab 0.0.11
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- blockchainlab.rb
+++ blockchainlab.rb
@@ -1,8 +1,8 @@
 class Blockchainlab < Formula
   desc "Blockchain Laboratory: Bitcoin experiments."
   homepage "https://github.com/danielfalbo/blockchainlab"
-  url "https://github.com/danielfalbo/blockchainlab/releases/download/v0.0.10/blockchainlab.jar"
-  sha256 "eed1cc5c7168f8fc1bab6d23a02e9710980cc721e64ba934a08b5156590815d3"
+  url "https://github.com/danielfalbo/blockchainlab/releases/download/v0.0.11/blockchainlab.jar"
+  sha256 "14682bb9e641c29cde7db9cc1f44d137eb18b193f52ac8cdf2550c46ab061c74"
 
   depends_on "openjdk@11"
 
